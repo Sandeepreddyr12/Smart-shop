a5ef5b762c3d50cabb0f5519125075e12128651d
sandeep reddy <sandeepreddyr12@gmail.com>
2025-08-30 21:06:53 +0530

    added spinner instead of text

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -1,12 +1,14 @@
 import { getTranslations } from 'next-intl/server'
+import Spinner from '@/components/shared/header/spinner'
 
 export default async function LoadingPage() {
   const t = await getTranslations()
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen '>
-      <div className='p-6 rounded-lg shadow-md w-1/3 text-center'>
-        {t('Loading.Loading')}
+    <div className="flex flex-col items-center justify-center min-h-screen  ">
+      <div className="p-6 rounded-lg shadow-md w-1/3 text-center bg-slate-200">
+        <Spinner />
+        <div> {t('Loading.Loading')}</div>
       </div>
     </div>
-  )
+  );
 }
