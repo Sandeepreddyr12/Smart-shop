da00bc518298359ca6c64132ae00afb6d6fecb13
sandeep reddy <sandeepreddyr12@gmail.com>
2025-08-30 21:06:00 +0530

    added system level session

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,28 +1,30 @@
-import { Geist, Geist_Mono } from 'next/font/google'
-import '../globals.css'
-import ClientProviders from '@/components/shared/client-providers'
-import { getDirection } from '@/i18n-config'
-import { NextIntlClientProvider } from 'next-intl'
-import { getMessages } from 'next-intl/server'
-import { routing } from '@/i18n/routing'
-import { notFound } from 'next/navigation'
-import { getSetting } from '@/lib/actions/setting.actions'
-import { cookies } from 'next/headers'
+import { Geist, Geist_Mono } from 'next/font/google';
+import '../globals.css';
+import ClientProviders from '@/components/shared/client-providers';
+import { getDirection } from '@/i18n-config';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { routing } from '@/i18n/routing';
+import { notFound } from 'next/navigation';
+import { getSetting } from '@/lib/actions/setting.actions';
+import { cookies } from 'next/headers';
+import CustomSessionProvider from '@/components/shared/session-provider';
+import { auth } from '@/auth';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
-})
+});
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
-})
+});
 
 export async function generateMetadata() {
   const {
     site: { slogan, name, description, url },
-  } = await getSetting()
+  } = await getSetting();
   return {
     title: {
       template: `%s | ${name}`,
@@ -30,27 +32,29 @@ export async function generateMetadata() {
     },
     description: description,
     metadataBase: new URL(url),
-  }
+  };
 }
 
 export default async function AppLayout({
   params,
   children,
 }: {
-  params: { locale: string }
-  children: React.ReactNode
+  params: { locale: string };
+  children: React.ReactNode;
 }) {
-  const setting = await getSetting()
-  const currencyCookie = (await cookies()).get('currency')
-  const currency = currencyCookie ? currencyCookie.value : 'USD'
+  const setting = await getSetting();
+  const currencyCookie = (await cookies()).get('currency');
+  const currency = currencyCookie ? currencyCookie.value : 'USD';
 
-  const { locale } = await params
+  const { locale } = await params;
   // Ensure that the incoming `locale` is valid
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!routing.locales.includes(locale as any)) {
-    notFound()
+    notFound();
   }
-  const messages = await getMessages()
+  const messages = await getMessages();
+
+  const session = await auth();
 
   return (
     <html
@@ -62,11 +66,13 @@ export default async function AppLayout({
         className={`min-h-screen ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NextIntlClientProvider locale={locale} messages={messages}>
-          <ClientProviders setting={{ ...setting, currency }}>
-            {children}
-          </ClientProviders>
+          <CustomSessionProvider session={session}>
+            <ClientProviders setting={{ ...setting, currency }}>
+              {children}
+            </ClientProviders>
+          </CustomSessionProvider>
         </NextIntlClientProvider>
       </body>
     </html>
-  )
+  );
 }
