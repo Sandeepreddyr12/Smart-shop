6a3464049b2a0a7c3b5bafa5e87191355097ae4d
sandeep reddy <sandeepreddyr12@gmail.com>
2025-09-04 21:34:09 +0530

    modified for modularity

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,30 +1,72 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+'use client';
 
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
-import { IProduct } from '@/lib/db/models/product.model'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { useSession } from 'next-auth/react';
 
-import Rating from './rating'
-import { formatNumber, generateId, round2 } from '@/lib/utils'
-import ProductPrice from './product-price'
-import ImageHover from './image-hover'
-import AddToCart from './add-to-cart'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from '@/components/ui/card';
+import { IProduct } from '@/lib/db/models/product.model';
+import {
+  useProductView,
+  // useAddToCart as useAddToCartInteraction,
+} from '@/hooks/use-user-interactions';
+
+import Rating from './rating';
+import { formatNumber, generateId, round2 } from '@/lib/utils';
+import ProductPrice from './product-price';
+import ImageHover from './image-hover';
+import AddToCart from './add-to-cart';
 
 const ProductCard = ({
   product,
   hideBorder = false,
   hideDetails = false,
   hideAddToCart = false,
+  onProductClick,
 }: {
-  product: IProduct
-  hideDetails?: boolean
-  hideBorder?: boolean
-  hideAddToCart?: boolean
+  product: IProduct;
+  hideDetails?: boolean;
+  hideBorder?: boolean;
+  hideAddToCart?: boolean;
+  onProductClick?: (product: IProduct) => void;
 }) => {
+  const { data: session } = useSession();
+  const { trackView } = useProductView();
+  // const { trackAddToCart } = useAddToCartInteraction();
+
+  const handleProductView = () => {
+    if (session?.user?.id) {
+      trackView(session.user.id, product._id, product.category);
+    }
+  };
+
+  // const handleAddToCartInteraction = () => {
+  //   if (session?.user?.id) {
+  //     trackAddToCart(session.user.id, product._id, 1);
+  //   }
+  // };
+
+  // console.log("product-card")
+
+  const handleNavigate = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    handleProductView();
+    if (onProductClick) {
+      e.preventDefault();
+      onProductClick(product);
+    }
+  };
+
   const ProductImage = () => (
-    <Link href={`/product/${product.slug}`}>
-      <div className='relative h-52'>
+    <Link href={`/product/${product.slug}`} onClick={handleNavigate}>
+      <div className="relative h-52">
         {product.images.length > 1 ? (
           <ImageHover
             src={product.images[0]}
@@ -32,25 +74,26 @@ const ProductCard = ({
             alt={product.name}
           />
         ) : (
-          <div className='relative h-52'>
+          <div className="relative h-52">
             <Image
               src={product.images[0]}
               alt={product.name}
               fill
-              sizes='80vw'
-              className='object-contain'
+              sizes="80vw"
+              className="object-contain"
             />
           </div>
         )}
       </div>
     </Link>
-  )
+  );
   const ProductDetails = () => (
-    <div className='flex-1 space-y-2'>
-      <p className='font-bold'>{product.brand}</p>
+    <div className="flex-1 space-y-2">
+      <p className="font-bold">{product.brand}</p>
       <Link
         href={`/product/${product.slug}`}
-        className='overflow-hidden text-ellipsis'
+        className="overflow-hidden text-ellipsis"
+        onClick={handleNavigate}
         style={{
           display: '-webkit-box',
           WebkitLineClamp: 2,
@@ -59,7 +102,7 @@ const ProductCard = ({
       >
         {product.name}
       </Link>
-      <div className='flex gap-2 justify-center'>
+      <div className="flex gap-2 justify-center">
         <Rating rating={product.avgRating} />
         <span>({formatNumber(product.numReviews)})</span>
       </div>
@@ -71,9 +114,9 @@ const ProductCard = ({
         forListing
       />
     </div>
-  )
+  );
   const AddButton = () => (
-    <div className='w-full text-center'>
+    <div className="w-full text-center">
       <AddToCart
         minimal
         item={{
@@ -89,16 +132,17 @@ const ProductCard = ({
           quantity: 1,
           image: product.images[0],
         }}
+        // onAddToCart={handleAddToCartInteraction}
       />
     </div>
-  )
+  );
 
   return hideBorder ? (
-    <div className='flex flex-col'>
+    <div className="flex flex-col">
       <ProductImage />
       {!hideDetails && (
         <>
-          <div className='p-3 flex-1 text-center'>
+          <div className="p-3 flex-1 text-center">
             <ProductDetails />
           </div>
           {!hideAddToCart && <AddButton />}
@@ -106,22 +150,22 @@ const ProductCard = ({
       )}
     </div>
   ) : (
-    <Card className='flex flex-col  '>
-      <CardHeader className='p-3'>
+    <Card className="flex flex-col  ">
+      <CardHeader className="p-3">
         <ProductImage />
       </CardHeader>
       {!hideDetails && (
         <>
-          <CardContent className='p-3 flex-1  text-center'>
+          <CardContent className="p-3 flex-1  text-center">
             <ProductDetails />
           </CardContent>
-          <CardFooter className='p-3'>
+          <CardFooter className="p-3">
             {!hideAddToCart && <AddButton />}
           </CardFooter>
         </>
       )}
     </Card>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
